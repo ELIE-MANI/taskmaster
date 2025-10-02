a502ee69acefa3c1599d5433f3dc74f0a01bf08c
ELIE-MANI <eliemaniraguha@gmail.com>
2025-10-02 17:10:39 +0200

    feature: implement routing

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return ( 
+    <>
+    <h1>This the Home Page</h1>
+    </>
+   );
+}
+
+export default HomePage;
---
+++ src/router/router.js
@@ -0,0 +1,13 @@
+import { createBrowserRouter } from "react-router";
+import HomePage from "../pages/HomePage";
+
+const router = createBrowserRouter([
+
+  {
+    path: "/",
+    Component: HomePage,
+  }
+
+])
+
+export default router;
