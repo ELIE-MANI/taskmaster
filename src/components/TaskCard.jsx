0b55ba1d7ca676e9856d1be9cb387ef7f4ade1d7
ELIE-MANI <eliemaniraguha@gmail.com>
2025-10-04 11:18:48 +0200

    feat: show task completed state

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -1,9 +1,9 @@
 function TaskCard({task} ) {
   return (  
-<div className="w-lg mx-auto my-2 p-2 bg-primary text-white rounded shadow-2xl">
+<div className="w-lg mx-auto my-2 p-2 bg-primary text-white rounded shadow-2xl flex justify-between items-center">
 <h2>{task.title}</h2>
 <span>
-  {/*task.completed ?"✅" :"❌"*/}
+  {task.completed ?"✅" :"❌ pending"}
 </span>
 </div>
 
